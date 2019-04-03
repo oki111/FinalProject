c96080b33e55d1ad5126026ea691912e02927d9b
oki111 <48803985+oki111@users.noreply.github.com>
2019-04-04 01:08:53 +0200

    Added projects, packages and mock database

---
+++ SalonVencanica.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SalonVencanica.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonVencanica.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
