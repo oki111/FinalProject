ad6e600e90565409f5bf2497564013aaff8039e6
unknown <okiolivera@gmail.com>
2019-04-08 01:16:39 +0200

    added basic shopping cart

---
+++ SalonVencanica.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SalonVencanica.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalonVencanica.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
